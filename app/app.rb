57e5607092f1f34684015817f2ecc3f44fa5592a
matt vickers <matt.vickers@sky.com>
2016-10-17 15:29:41 +0100

    assign key value variables

--- app/app.rb
+++ app/app.rb
@@ -2,12 +2,19 @@ require 'sinatra/base'
 require 'uri'
 
 class ServerTest < Sinatra::Base
+
+  $hashdump = []
+
   get '/' do
     'Hello ServerTest!'
   end
 
   get '/set' do
-    request.query_string
+    str = request.query_string
+    key = str.partition('=')[0]
+    value = str.partition('=')[2]
+    hash = {:key => key, :value => value}
+    puts hash
   end
 
   # start the server if ruby file executed directly
